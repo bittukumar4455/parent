5cd1c85c5afcc1d66607fa6679459ee9a81f7cc2
Bittu Kumar <122342849+bittukumar4455@users.noreply.github.com>
2023-08-11 12:19:55 +0530

    Update ip and port

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   let [first, setfirst] = useState(0);
   window.addEventListener('message', (event) => {
-    if (event.origin === 'http://localhost:3001'){
+    if (event.origin === 'http://3.21.211.144:5023'){
       if(typeof(event.data)==="number"){
         setfirst(event.data)
       }
@@ -17,7 +17,7 @@ return (
     <div className="App">
       This is a parent
       <h1>{first}</h1>
-      <iframe title='ParentFrame' src="http://localhost:3001/"></iframe>
+      <iframe title='ParentFrame' src="http://3.21.211.144:5023"></iframe>
     </div>
   );
 }
